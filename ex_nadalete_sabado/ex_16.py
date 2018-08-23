801c521e4503a65c3b940ebfa70415085cf3cd35
mateusmsouza <mateusmsouza@outlook.com>
2018-08-23 18:06:20 -0300

    Removing useless folder

--- ex_nadalete_sabado/ex_16.py
+++
@@ -1,6 +0,0 @@
-base = int(input("Insira a base:\n"))
-expoente = int(input('Insira o expoente\n'))
-acumula = base
-for i in range(1,expoente):
-	acumula = acumula * base
-print(acumula)
--- matematica discreta/princpio_boa_ordenacao.py
+++
@@ -1,7 +0,0 @@
-#encoding: utf-8
-
-print("Principio faz alguma coisa")
-lista = input("Entre com uma lista de números, separado por ';':\n")
-lista = lista.input(";")
-
-print(min(lista))
