aa8a57987effa17851e013472b987d9112634c09
Mateus Souza <mateus_msouza@outlook.com>
2018-09-15 12:07:10 -0300

    Lista 4

---
+++ 2sem/LP/Lista4/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <locale.h>
+
+#include "func.h"
+
+int main(){
+	int decisao;
+
+	printf("Insira um número ai vá.\n");
+	scanf("%d", &decisao);
+
+	switch(decisao){
+		case 1:exercicio1();
+				break;
+		case 2:exercicio2();
+				break;
+		case 3:exercicio3();
+				break;
+		case 4:exercicio4();
+				break;
+		case 5:exercicio5();
+				break;
+		case 6:exercicio6();
+				break;
+		case 7:exercicio7();
+				break;
+		case 8:exercicio8();
+				break;
+		case 9:exercicio9();
+				break;
+		case 10:exercicio10();
+				break;
+		case 11:exercicio11();
+				break;
+		case 22:exercicio22();
+				break;
+		case 23:exercicio23();
+				break;
+		case 24:exercicio24();
+				break;
+		case 25:exercicio25();
+				break;
+		case 26:exercicio26();
+				break;
+		case 27:exercicio27();
+				break;
+		case 28:exercicio28();
+				break;
+		case 29:exercicio28();
+				break;
+		case 30:exercicio30();
+				break;
+		case 31:exercicio31();
+				break;
+		case 32:exercicio32();
+				break;
+		case 33:exercicio33();
+				break;
+		case 34:exercicio34();
+				break;
+		case 35:exercicio35();
+				break;
+		case 36:exercicio36();
+				break;
+		case 37:exercicio37();
+				break;
+		case 38:exercicio38();
+				break;
+		case 39:exercicio39();
+				break;
+		case 40:exercicio40();
+				break;
+		case 41:exercicio41();
+				break;
+		case 42:exercicio42();
+				break;
+		case 43:exercicio43();
+				break;
+		case 44:exercicio44();
+				break;
+		case 45:exercicio45();
+				break;
+		case 46:exercicio46();
+				break;
+		case 47:exercicio47();
+				break;
+		case 48:exercicio48();
+				break;
+		case 49:exercicio49();
+				break;
+		case 50:exercicio50();
+				break;
+		case 51:exercicio51();
+				break;
+		default:printf("Errou fi\n");
+
+	}
+
+	return 0;
+
+}
