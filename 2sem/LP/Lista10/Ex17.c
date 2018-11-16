f207f72074a3601a5281994211429ad9202b67d8
mateusmsouza <mateusmsouza@outlook.com>
2018-11-16 17:19:47 -0200

    Criando construtor da matriz

--- 2sem/LP/Lista10/Ex17.c
+++ 2sem/LP/Lista10/Ex17.c
@@ -15,17 +15,68 @@ sa´ıda:
 */
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+    int numLinhas;
+    int numColunas;
+    int blankSpaces;
+}Matriz;
+Matriz matriz;
+
+void constructor(int numLinhas, int numColunas, int blankSpaces){
+    matriz.numLinhas = numLinhas;
+    matriz.numColunas = numColunas;
+    matriz.blankSpaces = blankSpaces;
+}
+
+void getFirstLine(char *coord, char linhaArquivo[], int valoresConstructor[], int countConstructor){
+    coord = strtok(linhaArquivo," "); // separando os valores da primeira linha pelo espaço
+    while(coord != NULL){ // iterando o ponteiro até que seja nulo
+        // convertendo o string para inteiro
+        valoresConstructor[countConstructor] = strtol(coord,NULL,10);
+            // Aumentando o valor do contador e trocando de valor do ponteiro.
+        countConstructor++;
+        coord = strtok(NULL, " ");
+    }
+
+    constructor(valoresConstructor[0],valoresConstructor[1],valoresConstructor[2]);
+}
+
+
+
 int main(){
+    setlocale(LC_ALL, "portuguese");
+    // Def
     FILE* input;
     char linhaArquivo[255];
-    int useOrNot;
+    char c;
+    char *coord;
+    int valoresConstructor[3];
+    int countConstructor = 0;
+    int line = 1;
+
+
 
     input = fopen("input.txt","r");
     while (fgets(linhaArquivo, sizeof(linhaArquivo), input) != NULL){
-        printf("%s", linhaArquivo);
+
+        if (line == 1){ // garantindo que apenas a primeira linha entrará no loop
+                getFirstLine(coord, linhaArquivo, valoresConstructor, countConstructor);
+        }
+        line++;
 
     }
+
+    // Construindo a matriz
+    for (int linhas = 0 ; linhas <= matriz.numColunas; linhas++){
+        for (int colunas = 0 ; colunas < matriz.numLinhas; colunas++){
+            printf("1 ");
+        }
+        printf("\n");
+    }
+
+
     return 0;
 }
