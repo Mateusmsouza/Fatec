841ead48ac0cc93ac28b16101d650bb6fc46c538
Mateus Souza <mateusmsouza@outlook.com>
2018-11-21 19:42:21 -0200

    Ex21 finalizado

---
+++ 2sem/LP/Lista10/Ex25.c
@@ -0,0 +1,8 @@
+Fac¸a um programa gerenciar uma agenda de contatos. Para cada contato armazene o
+nome, o telefone e o aniversario (dia e m ´ es). O programa deve permitir ˆ
+(a) inserir contato
+(b) remover contato
+(c) pesquisar um contato pelo nome
+(d) listar todos os contatos
+(e) listar os contatos cujo nome inicia com uma dada letra
+(f) imprimir os aniversariantes do mes.
