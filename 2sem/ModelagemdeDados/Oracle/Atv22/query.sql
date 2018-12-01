92636cead9f4075e91a6dbc3bf176af0c6fc6d03
Mateus Souza <mateus_msouza@outlook.com>
2018-12-01 21:50:21 -0200

    Finalizando query e schema

--- 2sem/ModelagemdeDados/Oracle/Atv22/query.sql
+++ 2sem/ModelagemdeDados/Oracle/Atv22/query.sql
@@ -1,19 +1,35 @@
 -- Inserir 3 registros de usuário (um deles com nome Jack)
+-- Ao menos um usuário não deve possuir foto. 
 INSERT INTO usr_usuario (usr_nome_completo, usr_email, usr_senha, usr_foto) VALUES ('Mateus', 'mateus_msouza@outlook.com','123456', NULL);
 INSERT INTO usr_usuario (usr_nome_completo, usr_email, usr_senha, usr_foto) VALUES ('Fabio', 'fabio_romeiro@outlook.com','333333', NULL);
-INSERT INTO usr_usuario (usr_nome_completo, usr_email, usr_senha, usr_foto) VALUES ('Jack', 'Jack@outlook.com','jackin', NULL);
+INSERT INTO usr_usuario (usr_nome_completo, usr_email, usr_senha, usr_foto) VALUES ('Jack', 'Jack@outlook.com','jackin', '/foto/user3.jpg');
 
 -- 6 filmes (associar 2 filmes a cada pessoa)
+-- Dois filmes devem possuir o mesmo nome, sendo o mais recente baseado no mais antigo. 
 
 INSERT INTO fle_filme (fle_nome, fle_ano_lancamento, fle_sinopse) VALUES ('Jackass', 2000, 'sobosta');
-INSERT INTO fle_filme (fle_nome, fle_ano_lancamento) VALUES ('Harry Potro', 1990);
+INSERT INTO fle_filme (fle_nome, fle_ano_lancamento, fle_fkfle_nome, fle_fkfle_lancamento) VALUES ('Harry Potro', 1990, 'Jackass', 2000);
 INSERT INTO fle_filme (fle_nome, fle_ano_lancamento) VALUES ('A origem', 2000);
 INSERT INTO fle_filme (fle_nome, fle_ano_lancamento) VALUES ('Os simpsons', 1990);
 INSERT INTO fle_filme (fle_nome, fle_ano_lancamento) VALUES ('Avatar', 2000);
-INSERT INTO fle_filme (fle_nome, fle_ano_lancamento) VALUES ('Vingadores', 2000);
+INSERT INTO fle_filme (fle_nome, fle_ano_lancamento, fle_fkfle_nome, fle_fkfle_lancamento) VALUES ('Vingadores', 2000, 'Avatar',2000);
 
-INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) VALUES (4.1, 'mateus_msouza@outlook.com', 'Jackass', 2000);
+INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) 
+VALUES (4.1, 'mateus_msouza@outlook.com', 'Jackass', 2000);
+INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) 
+VALUES (4.1, 'mateus_msouza@outlook.com', 'Harry Potro', 1990);
+INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) 
+VALUES (4.1, 'fabio_romeiro@outlook.com', 'A origem', 2000);
+INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) 
+VALUES (4.1, 'fabio_romeiro@outlook.com', 'Os simpsons', 1990);
+INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) 
+VALUES (4.1, 'Jack@outlook.com', 'Avatar', 2000);
+INSERT INTO ast_assiste(ast_nota, ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento) 
+VALUES (4.1, 'Jack@outlook.com', 'Vingadores', 2000);
 
--- Ao menos um usuário não deve possuir foto. 
--- Dois filmes devem possuir o mesmo nome, sendo o mais recente baseado no mais antigo. 
--- Algum usuário deve ser amigo de Jack;
+INSERT INTO amz_amizade(amz_usrfk_email1, amz_usrfk_email2) VALUES ('mateus_msouza@outlook.com', 'Jack@outlook.com');
+
+UPDATE fle_filme SET fle_sinopse = 'sinopse alterada' WHERE fle_sinopse = 'soubosta';
+DELETE FROM fle_sinopse WHERE fle_nome = 'A origem';
+SELECT * FROM usr_usuario WHERE usr_foto = NULL;
+SELECT * FROM amz_amizade WHERE amz_usrfk_email2 = 'Jack@outlook.com' OR amz_usrfk_email1 = 'Jack@outlook.com';
--- 2sem/ModelagemdeDados/Oracle/Atv22/schema.sql
+++ 2sem/ModelagemdeDados/Oracle/Atv22/schema.sql
@@ -1,50 +1,40 @@
--- adicionar constrants depois de criar as colunas
-CREATE TABLE usr_usuario(
-  usr_email VARCHAR2(100) NOT NULL, -- pk
-  usr_nome_completo VARCHAR2(100) NOT NULL,
-  usr_senha VARCHAR2(20) NOT NULL,
-  usr_foto VARCHAR2(50), -- pode ser null
+  -- adicionar constrants depois de criar as colunas
+  CREATE TABLE usr_usuario(
+    usr_email VARCHAR2(100) NOT NULL, -- pk
+    usr_nome_completo VARCHAR2(100) NOT NULL,
+    usr_senha VARCHAR2(20) NOT NULL,
+    usr_foto VARCHAR2(50), -- pode ser null
 
-  CONSTRAINT USR_PK PRIMARY KEY (usr_email)
-);
+    CONSTRAINT USR_PK PRIMARY KEY (usr_email)
+  );
 
-CREATE TABLE amz_amizade(
-  amz_usrfk_email1 VARCHAR2(100) NOT NULL, -- pk / fk
-  amz_usrfk_email2 VARCHAR2(100) NOT NULL,  -- pk / fk
-  
-  CONSTRAINT AMZ_PK1 PRIMARY KEY (amz_usrfk_email1),
-  CONSTRAINT AMZ_PK2 UNIQUE (amz_usrfk_email2),
-  CONSTRAINT AMZ_USR1 FOREIGN KEY (amz_usrfk_email1) REFERENCES usr_usuario (usr_email),
-  CONSTRAINT AMZ_USR2 FOREIGN KEY (amz_usrfk_email2) REFERENCES usr_usuario (usr_email)
-);
+  CREATE TABLE amz_amizade(
+    amz_usrfk_email1 VARCHAR2(100) NOT NULL, -- pk / fk
+    amz_usrfk_email2 VARCHAR2(100) NOT NULL,  -- pk / fk
+    
+    CONSTRAINT AMZ_PK1 PRIMARY KEY (amz_usrfk_email1, amz_usrfk_email2),
+    CONSTRAINT AMZ_USR1 FOREIGN KEY (amz_usrfk_email1) REFERENCES usr_usuario (usr_email),
+    CONSTRAINT AMZ_USR2 FOREIGN KEY (amz_usrfk_email2) REFERENCES usr_usuario (usr_email)
+  );
 
-CREATE TABLE fle_filme(
-  fle_nome VARCHAR2(100) NOT NULL, -- ser· pk
-  fle_ano_lancamento NUMBER(4) NOT NULL, -- ser· pk
-  fle_sinopse VARCHAR2(255), -- pode ser null 
-  fle_fkfle_nome VARCHAR2(100), -- ser· fk
-  fle_fkfle_lancamento NUMBER(4),
+  CREATE TABLE fle_filme(
+    fle_nome VARCHAR2(100) NOT NULL, -- ser√° pk
+    fle_ano_lancamento NUMBER(4) NOT NULL, -- ser√° pk
+    fle_sinopse VARCHAR2(255), -- pode ser null 
+    fle_fkfle_nome VARCHAR2(100), -- ser√° fk
+    fle_fkfle_lancamento NUMBER(4),
+    
+    CONSTRAINT FLE_PK PRIMARY KEY (fle_nome, fle_ano_lancamento)
+  );
 
-  CONSTRAINT FLE_PK1 PRIMARY KEY (fle_nome),
-  CONSTRAINT FLE_PK2 UNIQUE (fle_ano_lancamento),
-  
-  CONSTRAINT FLE_FK1 FOREIGN KEY (fle_fkfle_nome) REFERENCES fle_filme (fle_nome),
-  CONSTRAINT FLE_FK2 FOREIGN KEY (fle_fkfle_lancamento) REFERENCES fle_filme (fle_ano_lancamento)
-);
-
-CREATE TABLE ast_assiste(
-  ast_nota NUMBER(2,1) NOT NULL,
-  ast_usrfk_email VARCHAR2(100) NOT NULL, -- ser· fk
-  ast_flefk_nome VARCHAR2(100) NOT NULL, -- ser· fk
-  ast_flefk_ano_lancamento NUMBER(4) NOT NULL, -- ser· fk
-  
-  CONSTRAINT AST_PK PRIMARY KEY (ast_usrfk_email),
-  CONSTRAINT AST_PK2 UNIQUE (ast_flefk_nome),
-  CONSTRAINT AST_PK3 UNIQUE (ast_flefk_ano_lancamento),
-  
-  CONSTRAINT AST_FK FOREIGN KEY (ast_usrfk_email) REFERENCES usr_usuario (usr_email),
-  CONSTRAINT AST_FK2 FOREIGN KEY (ast_flefk_nome) REFERENCES fle_filme (fle_nome),
-  CONSTRAINT AST_FK3 FOREIGN KEY (ast_flefk_ano_lancamento) REFERENCES fle_filme (fle_ano_lancamento)
-
-);
+  CREATE TABLE ast_assiste(
+    ast_nota NUMBER(2,1) NOT NULL,
+    ast_usrfk_email VARCHAR2(100) NOT NULL, -- ser√° fk
+    ast_flefk_nome VARCHAR2(100) NOT NULL, -- ser√° fk
+    ast_flefk_ano_lancamento NUMBER(4) NOT NULL, -- ser√° fk
+    
+    CONSTRAINT AST_PK PRIMARY KEY (ast_usrfk_email, ast_flefk_nome, ast_flefk_ano_lancamento),
+    CONSTRAINT FK_PK FOREIGN KEY (ast_usrfk_email) REFERENCES usr_usuario (usr_email),
+    CONSTRAINT FK_PK1 FOREIGN KEY (ast_flefk_nome, ast_flefk_ano_lancamento) REFERENCES fle_filme (fle_nome, fle_ano_lancamento)
+  );
 
