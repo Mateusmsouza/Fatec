4844870f27d81932d0b59783bc7ef7d0f432e431
Mateus Souza <mateus_msouza@outlook.com>
2019-02-26 20:07:18 -0300

    Criando a matéria prog em bd

---
+++ 3sem/Estrutura de Dados/Exercicios de Lista Encadeada/vetorintolista.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct Lista{
+	int numElemento;
+	struct unidade *proxElemento;
+
+}unidade;
+
+int main(){
+	struct unidade *ponteiroUnidade;
+	int v[3];
+	v[0] = 1;
+	v[1] = 2;
+	v[2] = 3;
+
+	ponteiroUnidade = NULL;
+
+
+
+}
