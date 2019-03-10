d7f5c377544feb64ea57e3101d5d58fada5f292d
Mateus Souza <mateus_msouza@outlook.com>
2019-03-10 12:41:39 -0300

    criando mais funções de tratamento da LE e alterando para servir para lista encadeada sem cabeça também

--- 3sem/Estrutura de Dados/Exercicios de Lista Encadeada/ex02.c
+++ 3sem/Estrutura de Dados/Exercicios de Lista Encadeada/ex02.c
@@ -8,48 +8,71 @@ typedef struct registro {
 	struct registro *next;
 } celula;
 
-void busca(celula *le, int x){
-	if (le->objeto == x){
-		printf("Objeto: %d existe na lista\n", le->objeto);
+// função de busca na LE.
+celula *busca(celula *le, int x, int comcabeca){
+	if (comcabeca){
+		if (le != NULL){
+			if (le->objeto == x){
+				printf("Objeto: %d existe na lista\nRetornando ponteiro para o main...\n", le->objeto);
+				return le;
+			}
+			busca(le->next, x);
+		}	
 	}
-	if (le->next!=NULL) busca(le->next, x);
 }
 
+// função de imprime a LE inteira.
 void imprimir(celula *le){
-	if(le->next != NULL){
+	if(le != NULL){
 		printf("Objeto: %d\n", le->objeto);
 		imprimir(le->next);	
 	}else{
-		printf("Objeto: %d\n", le->objeto);
 		printf("Final da Lista Encadeada\n");
 	}
 
 }
 
-void insere(int novoElemento, celula *le){
+// Função para inserir um novo elemento no final da lista.
+void insereFinal(int novoElemento, celula *le){
 	if (le->next == NULL){
 		celula *novoObjeto = malloc(sizeof(celula));
 		novoObjeto->objeto = novoElemento;
 		novoObjeto->next = NULL;
 		le->next=novoObjeto;
 	}else{
-		insere(novoElemento, le->next);
+		insereFinal(novoElemento, le->next);
 	}
+}
+
+// Função para inserir um novo objeto entre o objeto P e seu próximo elemento
+void insereMeio(int novoElemento, celula *le){
+	if (le == NULL){
+		printf("Lista vazia, utilize a funcao insereFinal.\n");
+		return ;
+	}
+	celula *novaCelula;
+	novaCelula = malloc(sizeof(celula));
+	novaCelula->next = le->next;
+	novaCelula->objeto = novoElemento;
+	le->next = novaCelula;
+	printf("Elemento %d inserido com sucesso no endereco em memoria %p\n", novoElemento, &novaCelula);
 }
 
 int main(void){
 	celula *le;
-	le = malloc (sizeof(celula));
-	le->next = NULL;
+	//le = malloc (sizeof(celula));
+	le = NULL;
 
-	insere(1000000, le);
-	
-	insere(10000, le);
-	insere(1000, le);
-	insere(100, le);
-	insere(100, le);
-	
-	//imprimir(le);	
-	busca(le, 100);
+	insereFinal(1000000, le);
+	/*
+	insereFinal(10000, le);
+	insereFinal(1000, le);
+	insereFinal(100, le);
+	insereFinal(100, le);
+	*/
+	//imprimir(le->next);	
+	//busca(le, 100);
+	//insereMeio(666, busca(le, 1000));
+	//imprimir(le->next);
 	return EXIT_SUCCESS;
 }
