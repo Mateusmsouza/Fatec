468e358e1e80934933ddbda96ad9f087b1e08abb
Mateus Souza <mateus_msouza@outlook.com>
2019-03-26 09:06:01 -0300

    [ Iniciando lista 2 ]

--- 3sem/Estrutura de Dados/Lista 1/Ex08.c
+++ 3sem/Estrutura de Dados/Lista 1/Ex08.c
@@ -5,8 +5,9 @@ Its code consider dummy cell in the list.
 Mateus M Souza - 2019
 */
 #include <stdio.h>
+#include <stdlib.h>
 
-struct {
+struct LinkedList{
 
     int value;
     struct LinkedList *next;
@@ -16,6 +17,8 @@ struct {
 
 LinkedList *TOCOMSONO( LinkedList *lk1, LinkedList *lk2 ){
     LinkedList *lk = malloc( sizeof( LinkedList ) );
+    LinkedList *headcell = malloc( sizeof(LinkedList) );
+    headcell = lk;
     
     while( (lk1 != NULL) && (lk2 != NULL)){
       if(lk1 != NULL){
@@ -23,12 +26,36 @@ LinkedList *TOCOMSONO( LinkedList *lk1, LinkedList *lk2 ){
         lk = lk1;
         lk1 = lk1->next;
       }
+    //  printf("%d\n", lk->value);
 
       if(lk2 != NULL){
         lk->next = lk2;
         lk = lk2;
         lk2 = lk2->next;
       }
+
+    //  printf("%d\n", lk->value);
     }
-    return lk;
+    printf("%d\n", ((headcell->next)->next)->value);
+    return headcell;
+}
+
+int main(){
+  LinkedList *celula1 = malloc(sizeof(LinkedList));
+  celula1->value = 1;
+  LinkedList *celula2 = malloc(sizeof(LinkedList));
+  celula2->value = 3;
+  celula1->next = celula2;
+  celula2->next = NULL;
+
+  LinkedList *celula3 = malloc(sizeof(LinkedList));
+  celula3->value = 10;
+  LinkedList *celula4 = malloc(sizeof(LinkedList));
+  celula4->value = 11;
+  celula3->next = celula4;
+  celula4->next = NULL;
+
+  TOCOMSONO(celula1, celula3);
+
+  return 0;
 }
---
+++ 3sem/Estrutura de Dados/Lista 2/Ex01.c
@@ -0,0 +1,19 @@
+/*1- Faça uma função que retorne apenas uma lista encadeada
+
+ordenada com os elementos das duas listas encadeadas ordenadas.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct {
+
+    int value;
+    struct LinkedList *next;
+
+}typedef LinkedList;
+
+
+
+int main(){
+  return 0;
+}
