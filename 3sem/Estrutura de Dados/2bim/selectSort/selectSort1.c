e21c6b46b45d692d07292aa9e834f2560c2ed219
Mateus Souza <mateus_msouza@outlook.com>
2019-04-13 12:03:41 -0300

    [ IMPLEMENTANDO SELECT SORT 2 ]

--- 3sem/Estrutura de Dados/2bim/selectSort/selectSort1.c
+++ 3sem/Estrutura de Dados/2bim/selectSort/selectSort1.c
@@ -1,30 +1,38 @@
 /*
-*-----------
-* Implementando um select sort
-* Este select Sort selecionará o menor elemento em cada varredura e o posicionará no inicio do vetor
-*-----------
+*--------------------------------------------------------
+* Implementando um select sort                          *
+* Este select Sort selecionará o menor elemento em cada * 
+* varredura e o posicionará no inicio do vetor          *
+*-------------------------------------------------------*
+* Autor: Mateus M. Souza, 2019
 */
-#include <limits.h>
 #include <stdio.h>
 
 int *selectSort(int v[], int n){
   int menor;
   int swap;
 
+  // varrendo o vetor de 0 até o final
   for(int i = 0; i < n; i++)
   {
+    // definindo que o menor começa em I para que os elementos já ordenados não sejam trocados
     menor = i;
+
+    // Definindo J recebe I para que elementos já ordenados não sejam analisados na varredura
     for(int j = i; j < n; j++)
     {
-      if(v[menor] > v[j]){
-        menor = j;
-      }
+      // se o elemento de índice menor é maior que o elemento do índice J
+      // então menor recebe índice J
+      if(v[menor] > v[j]) menor = j;
     }
+
+    // Troca o menor elemento com o elemento que está no inicio do vetor.
     swap = v[menor];
     v[menor] = v[i];
     v[i] = swap;
   }
   
+  // printando o vetor para testes.
   for(int a = 0; a < n; a++)
   {
     printf("%d ", v[a]);
---
+++ 3sem/Estrutura de Dados/2bim/selectSort/selectSort2.c
@@ -0,0 +1,52 @@
+/*
+*----------------------------------------------------
+* Implementando um select sort                      *   
+* Este select Sort selecionará o maior elemento     *
+* em cada varredura e o posicionará no fim do vetor *
+*----------------------------------------------------
+* Autor: Mateus M. Souza, 2019
+*/
+#include <stdio.h>
+
+int *selectSort(int v[], int n){
+  int swap;
+  int aux;
+
+  // Iniciando o loop do final do vetor 
+  // e decrementando I que será a condição de parada do FOR mais interno
+  for(int i = n-1; i > -1; i--)
+  {
+
+    // Swap será o índice do maior elemento a cada uma das varreduras
+    swap = 0;
+
+    // Varrendo o vetor até (incluindo) o índice I.
+    // Esta varredurá impedirá que um elemento já ordenado seja analisado na varredura
+    // já que ela tem critério de parada J <= I
+    for(int j = 0; j <= i; j++)
+    {
+      // Caso elemento índice SWAP seja menor que elemento índice J, SWAP recebe J
+      if(v[swap] < v[j]) swap = j;
+    }
+
+    // A troca do maior elemento é realizado
+    // com o último elemento do vetor e o I será decrementado na próxima varredura.
+    aux = v[swap];
+    v[swap] = v[i];
+    v[i] = aux;
+  }
+
+  // printando o vetor para testes
+  for(int a = 0; a < n; a++)
+  {
+    printf("%d ", v[a]);
+  }
+  printf("\n");
+
+  return v;
+}
+
+int main(){
+  int v[] = {5,2,3,4,1};
+  selectSort(v,5);
+}
