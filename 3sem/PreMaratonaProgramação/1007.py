6ddeb1ad98310fa114055a0197d9ca66c73363e2
Mateus Souza <mateus_msouza@outlook.com>
2019-04-20 17:48:34 -0300

    [ Iniciando estudos para Maratona de Programação ]

---
+++ 3sem/PreMaratonaProgramação/1007.py
@@ -0,0 +1,14 @@
+'''
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B
+pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Entrada
+
+O arquivo de entrada contém 4 valores inteiros.
+'''
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+
+print("DIFERENCA = %d" %(A * B - C * D))
