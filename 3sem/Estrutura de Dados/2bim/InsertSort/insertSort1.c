e5d11ce1a002ee8cb77cba8c242584aba6276c52
Mateus Souza <mateus_msouza@outlook.com>
2019-04-22 17:56:22 -0300

    [ Finalizando implementação do Insert Sort ]

--- 3sem/Estrutura de Dados/2bim/InsertSort/insertSort1.c
+++ 3sem/Estrutura de Dados/2bim/InsertSort/insertSort1.c
@@ -18,31 +18,45 @@ void assign(){
     printf("| Faculdade de Tecnologia  de    |\n");
     printf("| São José dos Campos            |\n");
     printf("| Ano: 2019                      |\n");
-    printf("+--------------------------------+\n");
+    printf("+--------------------------------+\n\n");
 }
 
 void insertSort (int v[], int n){
+    // Criando variável de troca
+  int swap;
 
+  // iniciando a iteração a partir do primeiro elemento, o algoritmo exige que haja pelo menos um elemento anterior para
+  // realizar a comparação, por este motivo a iteração do for mais externo deve iniciar em 1.
   for(int i = 1; i < n; i++)
   {
-    int aux = v[i];
 
-    for(int j = i-1 ; v[j] < aux; i--)
+    // o j será iniciando sempre no elemento de índice anterior (i-1) em relação ao índice do for mais externo (i)
+    // a condição de parada é:
+    // 1 - o elemento do índice j não é maior do que seu sucessor j+1
+    // 2- o j é um número positivo ou neutro ( j >= 0) para evitar que as comparações saiam da medida do vetor
+    for(int j = i-1 ; v[j] > v[j+1] && j>=0; j--)
     {
-      v[i+1] = v[i];
+        // caso o elemento de j seja maior que o elemento de j+1
+        // a troca é realizada, já que isto significa que o elemento em J não está ordenado.
+        // j é decrementado para que este elemento continue sendo comparado até que se encontre
+        // o ponto exato onde ele estará ordenado no vetor.
+        swap = v[j+1];
+        v[j+1] = v[j];
+        v[j] = swap;
     }
-    v[i+1] = aux;
-    
+
   }
-  
+
 
 }
 void printVetor(int v[], int n){
     printf("\n");
+    printf("[");
     for(int a = 0; a < n; a++){
-        printf("%d ", v[a]);
+        if (a ==  n-1 )printf("%d", v[a]);
+        else printf("%d, ", v[a]);
     }
-    printf("\n");
+    printf("]\n");
 }
 
 int main(){
@@ -51,7 +65,12 @@ int main(){
   int tamanho = 5;
   int v[] = {5,2,3,4,1};
 
+  printf("Vetor original:\n");
+  printVetor(v, tamanho);
+
   insertSort(v, tamanho);
+
+  printf("Vetor ordenado por insert sort:\n");
   printVetor(v, tamanho);
   return 0;
 }
