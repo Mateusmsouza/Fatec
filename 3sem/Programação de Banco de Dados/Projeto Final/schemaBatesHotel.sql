b49ede7abb3a3c46ac3bae9a21575af201ca3a95
Mateus Machado <mateus_msouza@outlook.com>
2019-05-24 19:59:20 -0300

    [FIX] Corrigindo o schema Bates Hotel

--- 3sem/Programação de Banco de Dados/Projeto Final/schemaBatesHotel.sql
+++ 3sem/Programação de Banco de Dados/Projeto Final/schemaBatesHotel.sql
@@ -2,22 +2,25 @@ CREATE TABLE funcionario (
     fun_num_documento NUMBER(10) PRIMARY KEY,
     fun_data_contratacao DATE NOT NULL,
     fun_cargo CHAR(1) NOT NULL,
-    fun_turno CHAR(1) CONSTRAINT turno_fun CHECK('M','T','N'),
-    fun_salario NUMBER(10,2) NOT NULL,
-    fun_gestor FOREIGN KEY REFERENCES funcionario(fun_num_documento)
+    fun_turno CHAR(1) CONSTRAINT turno_fun CHECK(fun_turno in ('M','T','N')),
+    fun_salario NUMBER(10,2),
+    fun_gestor NUMBER(10),
+    CONSTRAINT funpk FOREIGN KEY (fun_gestor) REFERENCES funcionario(fun_num_documento)
 );
+
 
 CREATE TABLE cozinheiro (
     fun_num_documento NUMBER(10) PRIMARY KEY,
     coz_especialidade VARCHAR2(50),
-    CONSTRAINT num_doc_fun_coz FOREIGN KEY fun_num_documento REFERENCES funcionario(fun_num_documento)
+    CONSTRAINT num_doc_fun_coz FOREIGN KEY (fun_num_documento) REFERENCES funcionario(fun_num_documento)
 );
 
 CREATE TABLE camareira (
     fun_num_documento NUMBER(10) PRIMARY KEY,
-    cam_andares NUMBER(50),
-    CONSTRAINT num_doc_fun_cam FOREIGN KEY fun_num_documento REFERENCES funcionario(fun_num_documento)
+    cam_andares NUMBER(3),
+    CONSTRAINT num_doc_fun_cam FOREIGN KEY (fun_num_documento) REFERENCES funcionario(fun_num_documento)
 );
+
 CREATE TABLE CLIENTE (
 cli_nome varchar2(30) not null,
 cli_cpf number not null,
@@ -35,18 +38,11 @@ res_checkout Date not null,
 CONSTRAINT res_pk primary key (res_id)
 );
 
-create table QUARTO (
-qua_numero number not null,
-qua_preco number not null,
-qua_tipo varchar2(15) not null,
-qua_andar number not null,
-CONSTRAINT qua_pk primary key (qua_numero),
-CONSTRAINT qua_fk_andar foreign key (qua_andar) REFERENCES ANDAR(and_numero)
-);
+
+
 
 ALTER TABLE RESERVA ADD
-CONSTRAINT res_fk_cliente FOREIGN KEY (res_cli_cpf) REFERENCES CLIENTE(cli_cpf)
-;
+CONSTRAINT res_fk_cliente FOREIGN KEY (res_cli_cpf) REFERENCES CLIENTE(cli_cpf);
 
 create table GUARDA(
 gua_res_id number not null,
@@ -63,6 +59,23 @@ CREATE TABLE BTS_BATES(
     CONSTRAINT BTSPK PRIMARY KEY (BTS_CNPJ)
 );
 
+CREATE TABLE AND_ANDAR(
+    AND_NUMERO NUMBER(3),
+    BTS_CNPJ NUMBER(11),
+
+    CONSTRAINT ANDPK PRIMARY KEY (AND_NUMERO),
+    CONSTRAINT ANDFK FOREIGN KEY (BTS_CNPJ) REFERENCES BTS_BATES(BTS_CNPJ)
+
+);
+
+create table QUARTO (
+qua_numero number not null,
+qua_preco number not null,
+qua_tipo varchar2(15) not null,
+qua_andar number not null,
+CONSTRAINT qua_pk primary key (qua_numero),
+CONSTRAINT qua_fk_andar foreign key (qua_andar) REFERENCES AND_ANDAR(and_numero)
+);
 
 CREATE TABLE SLT_SALA_DE_TRABALHO(
     SLT_NUMERO NUMBER(3),
@@ -74,11 +87,3 @@ CREATE TABLE SLT_SALA_DE_TRABALHO(
     CONSTRAINT SLTFK FOREIGN KEY (BTS_CNPJ) REFERENCES BTS_BATES(BTS_CNPJ)
 );
 
-CREATE TABLE AND_ANDAR(
-    AND_NUMERO NUMBER(3),
-    BTS_CNPJ NUMBER(11),
-
-    CONSTRAINT ANDPK PRIMARY KEY (AND_NUMERO),
-    CONSTRAINT ANDFK FOREIGN KEY (BTS_CNPJ) REFERENCES BTS_BATES(BTS_CNPJ)
-
-);
