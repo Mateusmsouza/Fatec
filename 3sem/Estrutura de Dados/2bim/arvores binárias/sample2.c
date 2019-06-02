bda6adbc800c3c5d3c64e22d6ce5987c441fc2e7
Mateus Souza <mateus_msouza@outlook.com>
2019-06-01 22:31:16 -0300

    Criando funções de espelho

--- 3sem/Estrutura de Dados/2bim/arvores binárias/sample2.c
+++ 3sem/Estrutura de Dados/2bim/arvores binárias/sample2.c
@@ -20,13 +20,12 @@ no* retornaNo(int valor){
 
 // insere um novo nó em uma árvore de forma recursiva. Recebe ponteiro de ponteiro. Caso a árvore seja null, será criada um árvore nova
 void insereNoRecursivo(no **arvore, int valor){
-  printf("Ok\n");
   if (*arvore == NULL) *arvore = retornaNo(valor);
   else if (valor > (*arvore)->valor) insereNoRecursivo( &(*arvore)->direita, valor );
   else insereNoRecursivo( &(*arvore)->esquerda, valor );
 }
 
-// imprime uma arvore (esquerda, valor, direita) de forma recursiva
+// imprime uma arvore (esquerda, valor, direita) de forma recursiva.
 void imprimeArvoreRecursivo(no* arvore){
   if(arvore){
     imprimeArvoreRecursivo(arvore->esquerda);
@@ -35,11 +34,37 @@ void imprimeArvoreRecursivo(no* arvore){
   }
 }
 
+// retorna o ponteiro para o nó com o valor passado.
+no *buscaNaArvoreRecursiva(no *arvore, int valor){
+  if (arvore == NULL) return NULL;
+  else if ( arvore-> valor == valor) return arvore;
+  else return valor > arvore->valor ? buscaNaArvoreRecursiva(arvore->direita, valor) : buscaNaArvoreRecursiva(arvore->esquerda, valor);
+}
+
+// inverte todos os nós da árvore
+void espelhaArvore(no *arvore){
+  if (arvore){
+    espelhaArvore(arvore->direita);
+    espelhaArvore(arvore->esquerda);
+    no* aux = arvore->direita;
+    arvore->direita = arvore->esquerda;
+    arvore->esquerda = aux;
+    
+  }
+}
+
 int main(){
   no *arvorezinha = NULL;
 
   insereNoRecursivo(&arvorezinha, 10);
   insereNoRecursivo(&arvorezinha, 1);
+  insereNoRecursivo(&arvorezinha, 5);
+  insereNoRecursivo(&arvorezinha, 6);
+  insereNoRecursivo(&arvorezinha, 8);
+  imprimeArvoreRecursivo(arvorezinha);
+  printf("Endereco de memória do valor %d\n%p\n", buscaNaArvoreRecursiva(arvorezinha, 10)->valor, buscaNaArvoreRecursiva(arvorezinha, 10) );
+  
+  espelhaArvore(arvorezinha);
   imprimeArvoreRecursivo(arvorezinha);
   return 0;
 }
