20f44f7e6149af96c47bd25813b3465512c7c33b
Mateus Souza <mateus_msouza@outlook.com>
2019-06-06 21:59:51 -0300

    Insert Sort em lista encadeada

---
+++ 3sem/Estrutura de Dados/2bim/EstudosProva2/listaEncadeada.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+struct cel {
+    int valor;
+    struct cel *prox;
+}typedef cel;
+
+
+void inserir (cel **lista, int x) {
+    cel *temp = NULL, *aux = *lista;
+
+    temp = malloc(sizeof(cel));
+    temp->valor = x;
+    temp->prox = NULL;
+
+
+    if (*lista == NULL)
+      *lista = temp;
+    else {
+      for (; aux->prox != NULL; aux = aux->prox);
+      aux->prox = temp;
+    }
+ }
+
+ void imprimir(cel *lista) {
+    for (cel *aux = lista; aux != NULL; aux = aux->prox)
+        printf("%d ,", aux->valor);
+    printf("\n");
+}
+
+
+void 
+insertSort(cel *lst){
+    cel *temp = NULL;
+    cel *ordenado = lst->prox;
+
+    while(ordenado){
+        cel *valorOrdenando = ordenado->prox;
+
+        // removendo o valorOrdenado da lista
+        ordenado->prox = valorOrdenando->prox;
+
+        for (cel* i = lst; i != ordenado->prox; i = i->prox)
+        {
+            if (valorOrdenando->valor <= (i->prox)->valor || i->prox == ordenado->prox){
+                temp = i->prox;
+                i->prox = valorOrdenando;
+                valorOrdenando->prox = temp;
+
+                break;
+            }
+        }
+        ordenado = ordenado->prox;
+
+    }
+    temp = NULL;
+    free(temp);
+}
+/*
+cel retiracelula(cel *lista){
+	cel *removed = lista->prox;
+	lista->prox = removed->prox;
+	return removed;
+}
+
+void adicionaNaFrente(cel *lista, cel *adicionar){
+	cel *temp = lista->prox;
+	lista->prox = adicionar;
+	adicionar->prox = temp;
+}
+*/
+
+
+int main(){
+	cel *list = NULL;
+
+	inserir(&list, 3);
+	inserir(&list, 2);
+	inserir(&list, 1);
+	inserir(&list, 8);
+	inserir(&list, 6);
+    insertSort(list);
+	imprimir(list->prox);
+	return 0;
+}
