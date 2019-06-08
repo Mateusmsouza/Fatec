45c00dce8ac28c4dc2fd10fcbce8f2fedb7a57cf
Mateus Souza <mateus_msouza@outlook.com>
2019-06-08 12:56:15 -0300

    criando arquivo de ordenação complexidade n^2

--- 3sem/Estrutura de Dados/2bim/EstudosProva2/n2Sort.c
+++ 3sem/Estrutura de Dados/2bim/EstudosProva2/n2Sort.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* O algoritmo de ordenação pelo método da bolha é um dos piores.
+Sua lógica consiste em percorrer um vetor v[0..x] n^2 vezes e comparar 
+elemento ao seu próximo elemento (x e x+1), caso esteja desordenado, ordena-se estes dois elementos.
+Sua complexidade é de n^2.*/
 void
 bubbleSort(int v[], int n){
 	int temp;
-
 	for (int i = 0; i < n; ++i)
 	{
-		
 		for(int j = 0; j < n; j++){
-
 			if(v[j] >= v[j+1]){
 				temp = v[j];
 				v[j] = v[j+1];
 				v[j+1] = temp;
 			}
-
 		}
 	}
-
 }
 
 
